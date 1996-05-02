5a5d6eae124a5afc8c7fb1dc962e6e4be05ff647
John Bowman <bowman@ualberta.ca>
1996-05-02 18:55:25 +0000

    ''

--- utils.h
+++ utils.h
@@ -223,17 +223,9 @@ void out_function(ostream& os, T (*f)(int), char *text, int n, int nperline)
 {
 	int i;
 	os << "# " << text << endl;
-	if(nperline == 4) { // This case is optimized.
-		int ncont=((n-1)/nperline)*nperline;
-		for(i=0; i < ncont; i += nperline)
-			os << (*f)(i) << "\t" << (*f)(i+1) << "\t" << (*f)(i+2) << "\t"
-			   << (*f)(i+3) << " \\" << endl;
-		for(i=ncont; i < n-1; i++) os << (*f)(i) << "\t";
-	} else {
-		for(i=0; i < n-1;) {
-			os << (*f)(i);
-			if(++i % nperline) os << "\t"; else os << " \\" << endl;
-		}
+	for(i=0; i < n-1;) {
+		os << (*f)(i);
+		if(++i % nperline) os << "\t"; else os << " \\" << endl;
 	}
 	os << (*f)(n-1) << endl;
 }
@@ -248,17 +240,9 @@ void out_curve(ostream& os, T *f, char *text, int n, int nperline)
 {
 	int i;
 	os << "# " << text << endl;
-	if(nperline == 4) { // This case is optimized.
-		int ncont=((n-1)/nperline)*nperline;
-		for(i=0; i < ncont; i += nperline)
-			os << f[i] << "\t" << f[i+1] << "\t" << f[i+2] << "\t"
-			   << f[i+3] << " \\" << endl;
-		for(i=ncont; i < n-1; i++) os << f[i] << "\t";
-	} else {
-		for(i=0; i < n-1;) {
-			os << f[i];
-			if(++i % nperline) os << "\t"; else os << " \\" << endl;
-		}
+	for(i=0; i < n-1;) {
+		os << f[i];
+		if(++i % nperline) os << "\t"; else os << " \\" << endl;
 	}
 	os << f[n-1] << endl;
 }
