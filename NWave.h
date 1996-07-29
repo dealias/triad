81d7bd6ce05ff1ccf0933617775bd40ee9981e2c
John Bowman <bowman@ualberta.ca>
1996-07-29 20:22:10 +0000

    ''

--- NWave.h
+++ NWave.h
@@ -30,7 +30,7 @@ public:
 enum Linearity {STANDARD,EXPONENTIAL,CONSERVATIVE_EXPONENTIAL};
 
 class NWave : public ProblemBase {
-	void *Linearity(Var *, Var *, double);
+	void (*Linearity)(Var *, Var *, double);
 public:
 	void InitialConditions();
 	void Initialize();
