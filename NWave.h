1706a23f9d9cbc89d09d547651a9228e31d81ad6
John Bowman <bowman@ualberta.ca>
1996-07-29 20:29:42 +0000

    ''

--- NWave.h
+++ NWave.h
@@ -30,7 +30,7 @@ public:
 enum Linearity {STANDARD,EXPONENTIAL,CONSERVATIVE_EXPONENTIAL};
 
 class NWave : public ProblemBase {
-	Source_t (this::*Linearity);
+	Source_t (NWave::*Linearity);
 public:
 	void InitialConditions();
 	void Initialize();
@@ -38,7 +38,7 @@ public:
 	void Output(int it);
 	void FinalOutput();
 	
-	void LinearSrc(Var *src, Var *y, double t) {Linearity(src,y,t);}
+	void LinearSrc(Var *src, Var *y, double t) {this.Linearity(src,y,t);}
 	
 	Source_t StandardLinearity;
 	Source_t ExponentialLinearity;
