931f81e6a28be165a162f658fc44df475e225849
John Bowman <bowman@ualberta.ca>
1996-07-29 20:51:36 +0000

    ''

--- NWave.h
+++ NWave.h
@@ -38,7 +38,7 @@ public:
 	void Output(int it);
 	void FinalOutput();
 	
-	void LinearSrc(Var *src, Var *y, double t) {NWaveProblem->Linearity(src,y,t);}
+	void LinearSrc(Var *src, Var *y, double t);
 	
 	Source_t StandardLinearity;
 	Source_t ExponentialLinearity;
