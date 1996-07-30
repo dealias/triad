39f82bd5ac0a4fadd8b32811600529862c2bc146
John Bowman <bowman@ualberta.ca>
1996-07-30 16:22:08 +0000

    ''

--- Navier.cc
+++ Navier.cc
@@ -153,7 +153,7 @@ void NWave::InitialConditions()
 	
 	Geometry=NWave_Vocabulary.NewGeometry(geometry);
 	if(!Geometry->Valid(Problem->Abbrev()))
-		msg(ERROR,"Geometry \"%s\" is incompatible with Problem \"%s\"",
+		msg(ERROR,"Geometry \"%s\" is incompatible with Method \"%s\"",
 			Geometry->Name(),Problem->Abbrev());
 	Npsi=Geometry->Create();
 	ny=Npsi*(1+Nmoment);
