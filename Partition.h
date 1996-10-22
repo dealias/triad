be1c695f9a694514232eb4585f632ba53c1c2c0f
John Bowman <bowman@ualberta.ca>
1996-10-22 16:16:40 +0000

    ''

--- Partition.h
+++ Partition.h
@@ -431,7 +431,7 @@ INLINE void Partition<T,D>::Initialize() {
 	triadLimits[Nmode-1].stop=triad.Base()+Ntriad;
 
 	delete [] ntriad;
-	(void) weight.~DynVector();
+	weight.~DynVector();
 
 	cout << Ntriad << " WAVENUMBER TRIADS ALLOCATED." << endl;
 	if(verbose > 2) ListTriads(cout);
