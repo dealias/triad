13e80cafa0f2f32b8d426d36d77096c9a415dd5e
John Bowman <bowman@ualberta.ca>
1997-04-04 11:03:09 +0000

    ''

--- rfft.cc
+++ rfft.cc
@@ -1,5 +1,5 @@
 /* Derived Fast Fourier transform routines
-   Version 1.0 Copyright (C) 1997 John C. Bowman (bowman@ipp-garching.mpg.de)
+   Version 1.1 Copyright (C) 1997 John C. Bowman (bowman@ipp-garching.mpg.de)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
