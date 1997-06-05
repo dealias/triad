084b98f7d20466e76c769fc6b7c9137a0ddee91c
John Bowman <bowman@ualberta.ca>
1997-06-05 11:13:23 +0000

    ''

--- kernel.cc
+++ kernel.cc
@@ -350,8 +350,8 @@ void dump(int it, int final, double tmax)
 				rcheck << rname << "." << iter-microsteps << ends;
 				if(tmpdir) {
 					strstream buf;
-					buf << tmpdir << dirsep 
-						<< Vocabulary->FileName("","") << ends;
+					buf << tmpdir << dirsep << Vocabulary->FileName("","")
+						<< ends;
 					mkdir(buf.str(),0xFFFF);
 					rename(rname,rcheck.str());
 					msg(WARNING,"Cannot rename %s to checkpoint file %s",rname,
