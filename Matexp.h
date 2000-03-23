bf940293420800b287132d2b046095f517eb634d
John Bowman <bowman@ualberta.ca>
2000-03-23 19:13:32 +0000

    ''

--- Matexp.h
+++ Matexp.h
@@ -26,8 +26,10 @@ public:
 template<class T>
 const Array2<T> exp(const Array2<T>& A)
 {
-	double tolmax=1e-7;
-	double tolmin=9e-8;
+//	double tolmax=1e-7;
+//	double tolmin=9e-8;
+	double tolmax=1e-8;
+	double tolmin=9e-9;
 	
 //	double tolmax=1e-4;
 //	double tolmin=9e-5;
@@ -44,14 +46,15 @@ const Array2<T> exp(const Array2<T>& A)
 	
 	static RK5 Int;
 	Int.Allocate(n);
-	Int.SetParam(tolmax,tolmin,2.0,2.0,0.0,DBL_STD_MAX,INT_MAX,100,0,1);
+//	Int.SetParam(tolmax,tolmin,2.0,2.0,0.0,DBL_STD_MAX,INT_MAX,100,0,1);
+	Int.SetParam(tolmax,tolmin,2.0,2.0,0.0,DBL_STD_MAX,INT_MAX,100,0,0);
 		
 	for(unsigned int j=0; j < n; j++) {
 		for(unsigned int i=0; i < j; i++) y[i]=0.0;
 		y[j]=1.0;
 		for(unsigned int i=j+1; i < n; i++) y[i]=0.0;
 		
-		Real dt=1.0e-5;
+		Real dt=1.0e-3;
 		Real t=0.0;
 		int iteration=0;
 		Int.Integrate(MatExpProblem,y,t,1.0,dt,-1.0,iteration);
