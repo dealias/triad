4376d19ac4699add6a6b388bd908cca60ed89ba0
Malcolm Roberts <malcolm.i.w.roberts@gmail.com>
2000-07-20 21:38:01 +0000

    ''

--- DynVector.h
+++ DynVector.h
@@ -13,38 +13,38 @@ class DynVector
   mutable unsigned int alloc;
   mutable int state;
  public:
-  enum alloc_state {unallocated=0, allocated=1, temporary=2};
-  void Allocate(unsigned int s) {v=new T[alloc=s]; size=0; set(allocated);}
+  enum alloc_state {normal=0, temporary=1};
+  void Allocate(unsigned int s) {v=new T[alloc=s]; size=0;}
   void Deallocate() const {
-    if(test(allocated)) delete [] v;
-    size=0; alloc=0; clear(allocated);
+    if(alloc) delete [] v;
+    size=0; alloc=0;
   }
 	
   int test(int flag) const {return state & flag;}
   void clear(int flag) const {state &= ~flag;}
   void set(int flag) const {state |= flag;}
 	
-  DynVector() : v(NULL), size(0), alloc(0), state(unallocated) {}
+  DynVector() : v(NULL), size(0), alloc(0), state(normal) {}
   DynVector(const DynVector<T>& A) : v(A.v), size(A.size),
     alloc(A.alloc), state(A.test(temporary)) {}
   DynVector(unsigned int s) {Allocate(s);}
   ~DynVector() {Deallocate();}
 
-  void Freeze() {state=unallocated;}
-  void Hold() {if(test(allocated)) {state=temporary;}}
-  void Purge() const {if(test(temporary)) {Deallocate(); state=unallocated;}}
+  void Freeze() {state=normal;}
+  void Hold() {if(alloc) {state=temporary;}}
+  void Purge() const {if(test(temporary)) {Deallocate(); state=normal;}}
 	
   unsigned int Alloc() const {return alloc;}
   unsigned int Size() const {return size;}
 	
   void Resize(unsigned int i) {
-    if (i == 0 && alloc && test(allocated)) delete [] v;
+    if (i == 0 && alloc) Deallocate();
     else if(i > alloc) {
       T *v0=v;
       v=new T[i];
       if (size) {
 	for(unsigned int j=0; j < size; j++) v[j]=v0[j];
-	if(test(allocated)) delete [] v0;
+	if(alloc) delete [] v0;
       }
     }
     alloc=i;
@@ -52,9 +52,8 @@ class DynVector
   }
 
   void SetTop(unsigned int i){
-      if (alloc < i)
-	  Resize(i);
-      size = i;
+      if (alloc < i) Resize(i);
+      size=i;
   }
 
   T& operator [] (unsigned int i) {
@@ -78,12 +77,8 @@ class DynVector
   }
   
   void Pop(unsigned int i) {
-      if (size) 
-      {
-	  for (unsigned int j = i; j < size; j++)
-	  {
-	      v[j-1] = v[j];
-	  }
+      if(size) {
+	  for (unsigned int j=i; j < size; j++) v[j-1]=v[j];
 	  size--;
       }
   }
@@ -93,7 +88,7 @@ class DynVector
   void Load(T a) const {for(unsigned int i=0; i < size; i++) v[i]=a;}
   void Load(const T *a) const {memcpy(v,a,size*sizeof(T));}
   void Store(T *a) const {memcpy(a,v,size*sizeof(T));}
-  void Set(T *a) {v=a; clear(allocated);}
+  void Set(T *a) {v=a; alloc=0;}
 	
   DynVector<T>& operator = (T a) {Load(a); return *this;}
   DynVector<T>& operator = (const T *a) {Load(a); return *this;}
