8ac9d424883dafb02c387b86438d615175a35cc5
John Bowman <bowman@ualberta.ca>
2001-09-21 22:03:19 +0000

    ''

---
+++ ArrayL.cc
@@ -0,0 +1,29 @@
+#include <iostream.h>
+#include "ArrayL.h"
+
+using namespace Array;
+
+int main()
+{
+  uint n=5;
+	
+  array2L<double> A(n);
+
+  for(uint i=0; i < n; i++) {
+    for(uint j=0; j <= i; j++) A(i,j)=10*i+j;
+  }
+
+  cout << A << endl;
+	
+  array2L<double> B(n);
+	
+  // Optimized version:
+	
+  for(uint i=0; i < n; i++) {
+    array1<double> Bi=B[i];
+    for(uint j=0; j <= i; j++) Bi[j]=10*i+j;
+  }
+
+  cout << B << endl;
+   
+}   
