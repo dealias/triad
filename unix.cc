cdae03704c44562f44918475341f055a9f1e4a69
John Bowman <bowman@ualberta.ca>
2001-11-11 23:23:02 +0000

    ''

--- unix.cc
+++ unix.cc
@@ -32,7 +32,7 @@ typedef void stack_t;
 
 extern char* run;
 static const double init_time=time(NULL);
-static const double ticktime=1.0/CLK_TCK;
+static const double ticktime=1.0/CLOCKS_PER_SEC;
 
 void cputime(double *cpu)
 {
