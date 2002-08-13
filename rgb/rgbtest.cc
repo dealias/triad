4d826af68b63f36fb5e2eafa6ef45f8b479b1126
John Bowman <bowman@ualberta.ca>
2002-08-13 00:00:47 +0000

    ''

--- rgb/rgbtest.cc
+++ rgb/rgbtest.cc
@@ -3,16 +3,16 @@
 
 main()
 {
-	int Nx=16, Ny=16;
+	int Nx=256, Ny=256;
 
 	oxstream fout("data");
 	fout << Nx << Ny << 1;
 	
 	if(fout) 
-	  for(unsigned j=0; j < Ny; j++) {
+	  for(int j=Ny-1; j >= 0; j--) {
+	    for(unsigned j=0; j < Ny; j++) {
 	    for(unsigned i=0; i < Nx; i++) {
 	      fout << (float) (i+Nx*j);
-	    }
 	  }
 	fout.close();
 }
