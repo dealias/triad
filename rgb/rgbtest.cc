7b11d85f773cee1eacf9ef856bb9ca99066d94c2
John Bowman <bowman@ualberta.ca>
2002-08-13 00:01:36 +0000

    ''

--- rgb/rgbtest.cc
+++ rgb/rgbtest.cc
@@ -9,10 +9,10 @@ main()
 	fout << Nx << Ny << 1;
 	
 	if(fout) 
-	  for(int j=Ny-1; j >= 0; j--) {
-	    for(unsigned j=0; j < Ny; j++) {
+	  for(unsigned j=0; j < Ny; j++) {
 	    for(unsigned i=0; i < Nx; i++) {
 	      fout << (float) (i+Nx*j);
+	    }	
 	  }
 	fout.close();
 }
