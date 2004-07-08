b55ea56191508117184d8d1396453913019b34e3
John Bowman <bowman@ualberta.ca>
2004-07-08 03:33:50 +0000

    ''

--- fftw++.cc
+++ fftw++.cc
@@ -1,6 +1,6 @@
 #include "fftw++.h"
 
 bool fftw::Wise=false;
-const char *fftw::WisdomName="wisdom.txt";
+const char *fftw::WisdomName="wisdom3.txt";
 ifstream fftw::ifWisdom;
 ofstream fftw::ofWisdom;
