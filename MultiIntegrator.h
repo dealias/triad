d632ce61e313551de4d832b8d862205bdc5633f0
Malcolm Roberts <malcolm.i.w.roberts@gmail.com>
2010-06-03 19:25:53 +0000

    Move project from MultiIntegrator to Goy::MultiProblem

--- MultiIntegrator.h
+++ MultiIntegrator.h
@@ -13,16 +13,27 @@ class MultiProblem : public ProblemBase {
  protected:
   DynVector<unsigned> gridindex;
  public:
-  unsigned grid;
-  // Grid class goes here?
+  enum Field {Nfields};
+  unsigned Ngrids, grid, nfields;
+  // FIXME: Grid base class here?
+  virtual void Project(unsigned toG)=0;
+  vector3 mY; // FIXME: should this be public?
+  virtual void InitialConditions(unsigned Ngrids);
 };
+void MultiProblem::InitialConditions(unsigned Ngrids0) 
+{
+  Ngrids=Ngrids0;
+  Allocate(mY,Ngrids);
+}
 
-class MultiIntegratorBase : public IntegratorBase {
+class MultiIntegrator : public IntegratorBase {
  protected:
   Array1<RK *> Integrator;
   Array1<DynVector<unsigned> > nY;
+  //Note that ProblemBase already has a variable NY.
   MultiProblem *MProblem;
-  vector3 Y;
+  unsigned Nfields;
+  vector3 mY;
   vector2 YsaveUK; // for recovery from failed time steps
   bool Ysaved;
   unsigned grid;
@@ -34,27 +45,51 @@ class MultiIntegratorBase : public IntegratorBase {
   virtual void Prolong(unsigned)=0;
   virtual void Rescale()=0; // necessary?
 
+  const char *Name() {return "MultiIntegrator";}
+
   void Grid(unsigned int g) {
     grid=g;
     //GoyProblem->grid=g;
     MProblem->grid=g;
   }
 
+  void TimestepDependence() {
+    for (unsigned int i=0; i < Ngrids ; i++) {
+      Grid(i);
+      Integrator[i]->SetTime(t,dt);
+      Integrator[i]->TimestepDependence();
+    }
+  }
+
+  Solve_RC CheckError() {
+    bool adjust=true;
+    for (unsigned int i=0; i < Ngrids ; i++) {
+      Solve_RC rc=Integrator[i]->CheckError();
+      if(rc == UNSUCCESSFUL)
+	return UNSUCCESSFUL;
+      adjust &= (rc == ADJUST);
+    }
+    return adjust ? ADJUST : SUCCESSFUL;
+  }
 };
 
-void MultiIntegratorBase::Allocator(ProblemBase& problem,size_t)
+void MultiIntegrator::Allocator(ProblemBase& problem,size_t)
 {
   if(Ngrids < 2) msg(ERROR,"Need more grids");
   Allocate(Integrator,Ngrids);
   SetProblem(problem);
-
+  Nfields=MProblem->nfields;
+  
   Ysaved=0;
-  Allocate(Y,Ngrids);
-  Allocate(YsaveUK,Ngrids);
+  // FIXME: Y should poing to MProblem->Y
+  // FIXME: if Y gets swapped at any point, we're fucked.
+  Dimension(mY,MProblem->mY);
+  Allocate(YsaveUK,Ngrids); // FIXME: should this deal with more than just UK?
   
   Allocate(nY,Ngrids);
 
   // Assumes that sub-integrators are all the same order
+  // TODO: this could just take the lowest order and be fine.
   order=Integrator[0]->Order();
   pgrow=(order > 0) ? 0.5/order : 0;
   pshrink=(order > 1) ? 0.5/(order-1) : pgrow;
@@ -69,19 +104,26 @@ void MultiIntegratorBase::Allocator(ProblemBase& problem,size_t)
     Integrator[i]=integrator;
     Integrator[i]->SetProblem(problem);
     Integrator[i]->SetParam(*this);
+    mY[i].Allocate(Nfields);
+    for (unsigned F=0; F < Nfields; ++F) {
+      nY[i][F]=Problem->Size(Nfields*grid+F);
+      Dimension(mY[i][F],nY[i][F],Problem->YVector()[Nfields*grid+F]);
+    }
+    Integrator[i]->Allocator(mY[i],&nY[i],Problem->ErrorMask());
   }
   
   // this should also give an option for rescaling.
-  // I guess adding rescaling options to MultiIntegratorBase vocab or something?
-  //for (unsigned int i=1; i< Ngrids; i++) Project(i);
+  // I guess adding rescaling options to MultiIntegrator vocab or something?
 
-
-
-
+  // FIXME: Project should be moved out of the Integrator
+  // into the problem class, since it contains a bunch of grids
+  /*
+  for (unsigned int i=1; i< Ngrids; i++) 
+    MProblem->Project(i);
+  */
 }
 
-
-class GridBase {
+class Grid {
   //grid stuff, in the house!
 };
 
