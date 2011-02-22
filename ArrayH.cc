8c7fe2613882bb117aa9503b2ed8342cdb33befb
Malcolm Roberts <malcolm.i.w.roberts@gmail.com>
2011-02-22 10:55:40 +0000

    more work on ArrayH class.  New set(...) function auto-symmetrizes.

--- ArrayH.cc
+++ ArrayH.cc
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "Complex.h"
-#include "Arrayh.h"
-#include "ArrayL.h"
+#include "ArrayH.h"
 
 using namespace Array;
 using namespace std;
@@ -19,14 +18,18 @@ int main()
 
   int m=3;
   array2H<Complex> B(n,m);
-  for(unsigned i=0; i < n; ++ i) {
-    for(unsigned j=0; j < m; ++ j) {
-      B[i][j]=Complex(10+i,1+j);
+  for(int i=0; i < n; ++ i) {
+    for(int j=0; j < m; ++ j) {
+      //B[i][j]=Complex(10+i,1+j);
+      B.set(i,j,Complex(10+i,1+j));
     }
   }
-  cout << "\nB contains only" << endl;
+  B.set(0,0,Complex(1,1));
+  B.set(0,-2,Complex(0,1));
+
+  cout << "\nB contains" << endl;
   cout << B << endl;
-  cout << "\nbut, expanded, produces" << endl;
+  cout << "but, expanded, produces" << endl;
   for(int i=0; i < n; ++ i) {
     for(int j=-m+1; j < m; ++ j) {
       cout << B.get(i,j) << " ";
--- ArrayH.h
+++ ArrayH.h
@@ -22,6 +22,12 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 
 // Defining NDEBUG improves optimization.
 
+#ifdef NDEBUG
+#define __checkH(ix,iy)
+#else
+#define __checkH(ix,iy) this->CheckH(ix,iy)
+#endif
+
 #include "Array.h"
 
 namespace Array {
@@ -35,8 +41,7 @@ namespace Array {
     class array1H : public array1<T> {
   private:
     unsigned xorigin;
-    
-  public:
+   public:
     array1H() {}
     array1H(unsigned int nx0) {
       if(nx0 % 2 == 0) exit(1); // FIXME: improve error message
@@ -48,9 +53,7 @@ namespace Array {
       this->Dimension(nx0,v0);
     }
     T& operator [] (int ix) const {
-      if(noconj(ix,xorigin))
-	return this->v[ix];
-      //exit(1);  // FIXME: improve error message
+      // FIXME: return conjugate if ix < xorigin?
       return this->v[ix];
     }
     
@@ -63,7 +66,7 @@ namespace Array {
 	return this->v[ix];
       return conj(this->v[2*xorigin-ix]);
     }
-
+    
     array1H<T>& operator = (T a) {Load(a); return *this;}
   };
   
@@ -71,6 +74,26 @@ namespace Array {
     class array2H : public array2<T> {
   private:
     unsigned xorigin;
+    
+    virtual void CheckH(int ix, int iy) const {
+      //Check(ix,this->nx,2,1); // FIXME: segfaults
+      if(abs(iy) >= this->ny) {
+	std::ostringstream buf;
+	buf << "Array2H index ";
+	if(this->ny) buf << this->ny << " ";
+	buf << "is out of bounds (" << iy;
+	if(this->ny == 0) 
+	  buf << " index given to empty array";
+	else {
+	  if(iy > 0) buf << ">" << this->ny;
+	  else buf << "<" << -(int) (this->ny-1);
+	}
+	buf << ")";
+	ArrayExit(buf.str().c_str());
+      }
+    }
+  
+
   public:
     array2H() {}
     array2H(unsigned int nx0, unsigned int ny0) {
@@ -82,12 +105,17 @@ namespace Array {
       this->Dimension(nx0,ny0,v0);
     }
     array1H<T> operator [] (int ix) const {
+      // FIXME: return cc for certain cases?
       return array1H<T>(this->ny,this->v+ix*this->ny);
     }
     
-    T& operator () (int ix, int iy) const {return this->v[ix*this->ny+iy];}
+    T& operator () (int ix, int iy) const {
+      // FIXME: return cc for certain cases?
+      return this->v[ix*this->ny+iy];
+    }
     
     T get(int ix, int iy) {
+      __checkH(ix,iy);
       if(iy > 0)
 	return this->v[ix*this->ny+iy];
       if(iy == 0) {
@@ -95,8 +123,22 @@ namespace Array {
 	if(ix > xorigin) return this->v[ix*this->ny];
 	return this->v[(2*xorigin-ix)*this->ny].re;
       }
-      if(-iy > this->ny) exit(1); // FIXME: improve error message
       return conj(this->v[(2*xorigin-ix)*this->ny-iy]);
+    }
+
+    void set(int ix, int iy, T a) {
+      __checkH(ix,iy);
+      if(iy > 0) {this->v[ix*this->ny+iy]=a; return;}
+      if(iy == 0) {
+	if(ix == xorigin) {
+	  this->v[(2*xorigin-ix)*this->ny] = a.re; // FIXME: should be .re
+	} else {
+	  this->v[(2*xorigin-ix)*this->ny]=conj(a);
+	  this->v[ix*this->ny] = a;
+	}
+	return;
+      }
+      this->v[(2*xorigin-ix)*this->ny-iy]=conj(a);
     }
 
     array2H<T>& operator = (T a) {Load(a); return *this;}
