1780ddff75f17ef00ee6e0533699bce946ef0fd5
John Bowman <bowman@ualberta.ca>
2016-08-14 20:24:13 -0600

    Update license to GPL3.

--- kernel.cc
+++ kernel.cc
@@ -1,10 +1,10 @@
 /*                          T R I A D    
 An object-oriented C++ package for integrating initial value problems.
-Copyright (C) 2000 John C. Bowman (bowman@math.ualberta.ca)
+Copyright (C) 2000-2016 John C. Bowman (bowman@math.ualberta.ca)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
@@ -13,8 +13,8 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 */
 
 #include <omp.h>
