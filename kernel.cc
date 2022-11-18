99a50270c750734e622d834a89eb82e7802f864a
John Bowman <bowman@ualberta.ca>
2022-11-18 11:48:19 -0700

    Implement RK5DP.

--- kernel.cc
+++ kernel.cc
@@ -1,4 +1,4 @@
-/*                          T R I A D    
+/*                          T R I A D
 An object-oriented C++ package for integrating initial value problems.
 Copyright (C) 2000-2016 John C. Bowman (bowman@math.ualberta.ca)
 
@@ -91,7 +91,7 @@ static const char *tmpdir=tempdir();
 VocabularyBase::VocabularyBase()
 {
   Vocabulary=this;
-	
+
   VOCAB(itmax,(long long) 0,LLONG_MAX,"Maximum number of iterations");
   VOCAB(microsteps,1,INT_MAX,"Number of iterations per output step");
   VOCAB(microfactor,0.0,REAL_MAX,"Microstep growth factor per output step");
@@ -121,10 +121,10 @@ VocabularyBase::VocabularyBase()
   VOCAB(output,0,1,"");
   VOCAB_NOLIMIT(method,"");
   VOCAB_NOLIMIT(integrator,"");
-	
+
   ProblemTable=new Table<ProblemBase>("method");
   IntegratorTable=new Table<IntegratorBase>("integrator");
-	
+
   INTEGRATOR(Exact);
   INTEGRATOR(Euler);
   INTEGRATOR(Midpoint);
@@ -139,11 +139,12 @@ VocabularyBase::VocabularyBase()
   INTEGRATOR(RK3C);
   INTEGRATOR(RK4);
   INTEGRATOR(RK5);
+  INTEGRATOR(RK5DP);
   INTEGRATOR(SYM1);
   INTEGRATOR(SYM2);
 }
 
-void adjust_parameters(double& dt, double& dtmax, double& tmax, 
+void adjust_parameters(double& dt, double& dtmax, double& tmax,
 		       long long& itmax)
 {
   if(dt == 0.0) {
@@ -151,60 +152,60 @@ void adjust_parameters(double& dt, double& dtmax, double& tmax,
     if(itmax == -1) itmax=100;
     dt=fabs(tmax/(itmax ? itmax : 1));
   }
-	
-  if(tmax == 0.0) tmax=DBL_STD_MAX; 
+
+  if(tmax == 0.0) tmax=DBL_STD_MAX;
   if(itmax == -1) itmax=LLONG_MAX;
 
   if(dtmax == 0.0) dtmax=DBL_STD_MAX;
-	
+
   if(dt < dtmin) dt=dtmin;
   if(dt > dtmax) dt=dtmax;
-}	
+}
 
 int main(int argc, char *argv[])
 {
   int i;
-	
+
   inform=mailuser;
   cout.precision(REAL_DIG);
-  
+
   const int nstate=256;
   static char state[nstate];
   initstate(1,state,nstate);
-	
+
   cout << newl << PROGRAM << " version " << VERSION
        << " [(C) John C. Bowman 2000]" << newl;
-	
+
   cout << newl << "MACHINE: " << machine() << " [" << date() << "]" << newl;
-	
+
   cout << newl << "PROBLEM: " << Vocabulary->Name() << " (version "
        << ProblemVersion << ")" << newl;
-	
+
   cout << newl << "COMMAND LINE: ";
   for(i=1; i < argc; i++) cout << argv[i] << " ";
   cout << endl;
-	
+
   fpu_trap();
-  
+
   Vocabulary->Sort();
-	
+
   // Do a preliminary parse of command line to obtain parameter file name.
   for(i=1; i < argc; i++) Vocabulary->Assign(argv[i],0);
   msg_override=override;
-	
+
   // Allow time step to be overridden from command line (even on restarts).
-  if(dt) explicit_dt=1; 
-	
+  if(dt) explicit_dt=1;
+
   if(*run == 0) {run="test"; testing=1;}
-	
+
   lname=Vocabulary->FileName(dirsep,"LOCK");
   rname=Vocabulary->FileName(dirsep,"restart");
   rtemp=Vocabulary->FileName(dirsep,"restart=");
   pname=Vocabulary->FileName(dirsep,"p");
   ptemp=Vocabulary->FileName(dirsep,"p=");
-	
+
   fparam.open(pname);
-	
+
   if(fparam) {
     string s;
     ostringstream buf;
@@ -217,30 +218,30 @@ int main(int argc, char *argv[])
   } else {
     if(!testing)
       msg(OVERRIDE_GLOBAL,"Parameter file %s could not be opened",
-	  pname); 
+	  pname);
     errno=0;
     mkdir(Vocabulary->FileName(dirsep),0xFFFF);
   }
-	
+
   for(i=1; i < argc; i++) Vocabulary->Assign(argv[i]);
   adjust_parameters(dt,dtmax,tmax,itmax);
   cout << newl << "PARAMETERS:" << newl << newl;
   Vocabulary->List(cout);
-	
+
   fpu_trap(!dynamic);
   cout.precision(digits);
-	
+
   Problem=Vocabulary->NewProblem(method);
   Integrator=Vocabulary->NewIntegrator(integrator);
-	
+
   if(!(restart || initialize)) {
     fin.open(rname);
     if(fin && !clobber && !testing)
       msg(OVERRIDE_GLOBAL,"Restart file %s already exists",rname);
-    fin.close();	
+    fin.close();
     errno=0;
   }
-	
+
   if(restart) testlock();
 
   if(!restart && initialize) {
@@ -258,20 +259,20 @@ int main(int argc, char *argv[])
     const string& s=buf.str();
     mkdir(s.c_str(),0xFFFF);
   }
-					
+
   SaveParameters();
   if(threads == 0) threads=omp_get_max_threads();
   cout << newl << "THREADS: " << threads << endl;
   t=0.0; nout=0;
   Problem->InitialConditions();
-  
+
   Dimension(y,Problem->yVector());
   ny=Problem->Size();
-  
+
   if(restart || initialize) read_init();
   if(restart && dynamic < 0) dynamic=1;
   if(!restart) Problem->Initialize();
-  
+
   for(int i=0; i < 2; ++i) {
     open_output(gparam,(i == 0 ? "" : dirsep),"param.asy",0);
     Vocabulary->GraphicsDump(gparam,i);
@@ -281,32 +282,32 @@ int main(int argc, char *argv[])
   Problem->Setup();
   Integrator->SetParam(tolmax,tolmin,stepfactor,stepnoninvert,dtmin,dtmax,
 		       itmax,microsteps,microfactor,verbose,dynamic);
-	
+
   Integrator->Allocator(*Problem,Problem->align);
-  	
+
   cout << newl << "INTEGRATING:" << endl;
   set_timer();
-	
+
   Integrator->Integrate(t,tmax,dt,sample,iteration,nout);
-	
+
   Problem->FinalOutput();
-	
+
   cout << newl;
   cout << "COMPUTATION TERMINATED after " << iteration <<	" iteration" <<
     PLURAL(iteration) << newl << "                       at t=" <<
-    t << " with dt=" << dt << "." << newl; 
+    t << " with dt=" << dt << "." << newl;
   if(total_invert_cnt)
     cout << "NONINVERTIBILE TRANSFORMATION encountered " <<
       total_invert_cnt << " time" << PLURAL(total_invert_cnt) <<
       "." << newl;
-	
+
   cout << newl;
   cout << "INTEGRATION TIMING STATISTICS:" << newl <<	"            CPU = "
        << cpu[0] << ", CHILD = " << cpu[1] <<  ", SYS = " << cpu[2] << newl;
-	
+
   cout << newl << "DYNAMIC MEMORY ALLOCATED: " << memory() << " bytes ["
-       << date() << "]" << newl << endl; 
-	
+       << date() << "]" << newl << endl;
+
   if(!testing) mailuser("completed");
   return exit_signal;
 }
@@ -320,7 +321,7 @@ void read_init()
   const char *ny_msg=
     "Current value of ny (%d) disagrees with value (%d) in file\n%s. Perhaps you need to specify oldversion=%d";
   int init_version=0;
-	
+
   ixstream finit(rname);
   if(finit) {
     cout << newl << "READING " << upcase(type) << " DATA FROM FILE " << rname;
@@ -329,22 +330,22 @@ void read_init()
       finit >> RestartProblemVersion;
       cout << " (version " << RestartProblemVersion << ")";
     }
-    cout << "." << endl; 
+    cout << "." << endl;
     finit >> t0 >> dt0;
-    
+
     if(init_version < 3) {
       int final_iteration0;
       finit >> final_iteration0;
       final_iteration=final_iteration0;
     } else finit >> final_iteration;
-    
+
     for(i=0; i < ncputime; i++) finit >> cpu_restart[i];
     finit >> ny0;
     if(ny0 != ny) msg(OVERRIDE_GLOBAL,ny_msg,ny,ny0,rname,!oldversion);
     for(i=0; i < min(ny,ny0); i++) finit >> y[i];
     if(!finit)	msg(ERROR,"Cannot read from %s file %s",type,rname);
   } else msg(ERROR,"Could not open %s file %s",type,rname);
-	
+
   if(!explicit_dt) dt=dt0;
   if(restart) {t=t0; nout=nout0; last_dump=t;}
 }
@@ -361,7 +362,7 @@ void unlock()
   if(testing) return;
   if(flock) {
     flock.close();
-    if(remove(lname) == -1) 
+    if(remove(lname) == -1)
       msg(WARNING,"Could not remove lock file %s",lname);
   }
 }
@@ -378,7 +379,7 @@ void testlock()
   } else errno=0;
 }
 
-void dump(double t, int it, int final, double tmax) 
+void dump(double t, int it, int final, double tmax)
 {
   if((!restart || it > 0) && (tmax-t >= tprecision*tmax || final) &&
      t > last_dump) {
@@ -395,9 +396,9 @@ void SaveParameters() {
   Vocabulary->Dump(fdump);
   fdump.close();
   if(fdump) {
-    if(rename(ptemp,pname)) 
+    if(rename(ptemp,pname))
       msg(WARNING,"Cannot rename parameter file %s",ptemp);
-  }	
+  }
   else msg(WARNING,"Cannot write to parameter file %s",ptemp);
 }
 
@@ -418,7 +419,7 @@ void set_timer()
 	   << " " << setw(w) << "invert_cnt"
 	   << " " << setw(e) << "CPU"
 	   << " " << setw(e) << "CHILD"
-	   << " " << setw(e) << "SYS" 
+	   << " " << setw(e) << "SYS"
 	   << " " << setw(w) << "memory"
 	   << " " << setw(e) << "cpu" << endl;
   }
@@ -429,14 +430,14 @@ void statistics(double t, double dt, int it)
   int i;
   if(restart && it == 0) return;
   static double lastcputime=0.0;
-  
-	
+
+
   static int last_iter=0;
   long long iter=final_iteration+iteration;
 
   cputime(cpu);
   for(i=0; i < ncputime; i++) cpu[i] -= cpu0[i];
-  
+
   oxstream frestart(rtemp);
   if(frestart) {
     int i;
@@ -467,23 +468,23 @@ void statistics(double t, double dt, int it)
       msg(SLEEP,"Cannot write to restart file %s",rtemp);
       statistics(t,dt,it); // Try again;
       return;
-    }		  
+    }
   } else if(it == 0) msg(ERROR,"Could not open restart file %s",rtemp);
-	
+
   lock();
-  fstats << setw(w) << it << " " << iter << " " << setw(e) << t << " " 
+  fstats << setw(w) << it << " " << iter << " " << setw(e) << t << " "
 	 << setw(e) << dt << " " << setw(w) << invert_cnt << " ";
 
   total_invert_cnt += invert_cnt;
   invert_cnt=0;
-	
+
   for(i=0; i < ncputime; i++) {
     fstats << setw(e) << cpu_restart[i]+cpu[i] << " ";
   }
   fstats << setw(w) << memory() << " "
 	 << setw(e) << cpu[0]-lastcputime << endl;
   lastcputime=cpu[0];
-  
+
   if(!fstats) msg(WARNING,"Cannot write to statistics file");
   unlock();
 }
@@ -502,4 +503,4 @@ void check_compatibility(const bool debug)
 {
   const char *incompatible="Compiled with incompatible debugging modes";
   if(debug != DEBUG) msg(ERROR,incompatible);
-}  
+}
