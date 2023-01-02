ef71a667d11a38d31d0b83c4cd0bea4f29a6151f
John Bowman <bowman@ualberta.ca>
2023-01-02 09:29:52 -0700

    Apply parallelization threshold to conservative integrators.

--- Conservative.h
+++ Conservative.h
@@ -34,9 +34,11 @@ public:
       RK::Stage(s,start,stop);
       if(s < Astages-2) {
         Array::Array1<Var>::opt yss=ys[s];
-#pragma omp parallel for num_threads(threads)
-	for(size_t j=start; j < stop; j++)
-	  yss[j]=y[j];
+        PARALLELIF(
+          stop-start > threshold,
+          for(size_t j=start; j < stop; j++)
+            yss[j]=y[j];
+          );
       }
       RK::Stage(s,startT,stopT);
       PredictorSource(s);
@@ -67,69 +69,71 @@ public:
       } else {
 	rvector as=a[laststage];
         bool cont=true;
-#pragma omp parallel for num_threads(threads)
-	for(size_t j=start; j < stop; j++) {
-          if(cont) {
-            Var y0j=y0[j];
-            Var Skj=vsource[0][j];
-            Var temp=y0j+as[0]*Skj;
-            Var yS=product(y0j,Skj);
-            Var discr=as[0]*yS;
-            size_t k;
-            for(k=1; k < laststage; k++) {
-              Var Skj=vsource[k][j];
+        PARALLELIF(
+          stop-start > threshold,
+          for(size_t j=start; j < stop; j++) {
+            if(cont) {
+              Var y0j=y0[j];
+              Var Skj=vsource[0][j];
+              Var temp=y0j+as[0]*Skj;
+              Var yS=product(y0j,Skj);
+              Var discr=as[0]*yS;
+              size_t k;
+              for(k=1; k < laststage; k++) {
+                Var Skj=vsource[k][j];
+                temp += as[k]*Skj;
+                Var yS=product(ys[k-1][j],Skj);
+                discr += as[k]*yS;
+              }
+              Skj=vsource[k][j];
               temp += as[k]*Skj;
-              Var yS=product(ys[k-1][j],Skj);
-              discr += as[k]*yS;
+              yS=product(y[j],Skj);
+              discr=product(y0j,y0j)+2.0*(discr+as[k]*yS);
+              Var val;
+              if(nonnegative(discr))
+                val=signedsquareRoot(discr,temp);
+              else {
+                if(hybrid) {
+                  val=temp;
+                  discr=product(temp,temp);
+                } else {cont=false; continue;}
+              }
+              if(!Array::Active(errmask) || errmask[j])
+                CalcError(y0j,val,y[j],val);
+              y[j]=val;
             }
-            Skj=vsource[k][j];
-            temp += as[k]*Skj;
-            yS=product(y[j],Skj);
-            discr=product(y0j,y0j)+2.0*(discr+as[k]*yS);
-            Var val;
-            if(nonnegative(discr))
-              val=signedsquareRoot(discr,temp);
-            else {
-              if(hybrid) {
-                val=temp;
-                discr=product(temp,temp);
-              } else {cont=false; continue;}
-            }
-            if(!Array::Active(errmask) || errmask[j])
-              CalcError(y0j,val,y[j],val);
-            y[j]=val;
-          }
-	}
+          });
         if(!cont) return 0;
       }
     } else {
       rvector as=a[laststage];
-        bool cont=true;
-#pragma omp parallel for num_threads(threads)
-      for(size_t j=start; j < stop; j++) {
-        if(cont) {
-          Var y0j=y0[j];
-          Var Skj=vsource[0][j];
-          Var temp=y0j+as[0]*Skj;
-          Var discr=as[0]*product(y0j,Skj);
-          size_t k;
-          for(k=1; k < laststage; k++) {
-            Var Skj=vsource[k][j];
+      bool cont=true;
+      PARALLELIF(
+        stop-start > threshold,
+        for(size_t j=start; j < stop; j++) {
+          if(cont) {
+            Var y0j=y0[j];
+            Var Skj=vsource[0][j];
+            Var temp=y0j+as[0]*Skj;
+            Var discr=as[0]*product(y0j,Skj);
+            size_t k;
+            for(k=1; k < laststage; k++) {
+              Var Skj=vsource[k][j];
+              temp += as[k]*Skj;
+              discr += as[k]*product(ys[k-1][j],Skj);
+            }
+            Skj=vsource[k][j];
             temp += as[k]*Skj;
-            discr += as[k]*product(ys[k-1][j],Skj);
+            discr=product(y0j,y0j)+2.0*(discr+as[k]*product(y[j],Skj));
+            if(nonnegative(discr))
+              y[j]=signedsquareRoot(discr,temp);
+            else {
+              if(hybrid)
+                y[j]=temp;
+              cont=false;
+            }
           }
-          Skj=vsource[k][j];
-          temp += as[k]*Skj;
-          discr=product(y0j,y0j)+2.0*(discr+as[k]*product(y[j],Skj));
-          if(nonnegative(discr))
-            y[j]=signedsquareRoot(discr,temp);
-          else {
-            if(hybrid)
-              y[j]=temp;
-            cont=false;
-          }
-        }
-      }
+        });
       if(!cont) return 0;
     }
 
@@ -143,17 +147,20 @@ public:
     double halfdt=0.5*this->dt;
 
     if(dynamic) {
-#pragma omp parallel for num_threads(threads)
-      for(size_t j=startM; j < stopM; j++) {
-	Var val=y0[j]+halfdt*(source0[j]+source[j]);
-	if(!Array::Active(this->errmask) || this->errmask[j])
-	  CalcError(y0[j],val,y0[j]+this->dt*source0[j],val);
-	y[j]=val;
-      }
+      PARALLELIF(
+        stopM-startM > threshold,
+        for(size_t j=startM; j < stopM; j++) {
+          Var val=y0[j]+halfdt*(source0[j]+source[j]);
+          if(!Array::Active(this->errmask) || this->errmask[j])
+            CalcError(y0[j],val,y0[j]+this->dt*source0[j],val);
+          y[j]=val;
+        });
     } else {
-#pragma omp parallel for num_threads(threads)
-      for(size_t j=startM; j < stopM; j++)
-	y[j]=y0[j]+halfdt*(source0[j]+source[j]);
+      PARALLELIF(
+        stopM-startM > threshold,
+        for(size_t j=startM; j < stopM; j++)
+          y[j]=y0[j]+halfdt*(source0[j]+source[j]);
+        );
     }
     return 1;
   }
